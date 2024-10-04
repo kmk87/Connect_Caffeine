02863140ba72dc52f2e2c8a5e4caa5f54669f4c4
cho-gyeon-won <rudvkf2001@gmail.com>
2024-10-04 17:46:50 +0900

    [조경원] 채팅 최종 (2024-10-04)

--- src/main/java/com/cc/chatting/controller/ChattingViewController.java
+++ src/main/java/com/cc/chatting/controller/ChattingViewController.java
@@ -1,27 +1,33 @@
 package com.cc.chatting.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.cc.chatting.domain.ChatInviteDto;
 import com.cc.chatting.domain.ChatMessageDto;
 import com.cc.chatting.domain.ChatRoomDto;
 import com.cc.chatting.domain.ChatRoomVo;
 import com.cc.chatting.service.ChattingService;
+import com.cc.tree.service.OrgService;
 
 @Controller
 public class ChattingViewController {
 	
 	private final ChattingService chattingService;
+	private final OrgService orgService;
 	
 	@Autowired
-	public ChattingViewController(ChattingService chattingService) {
+	public ChattingViewController(ChattingService chattingService,
+			OrgService orgService) {
 		this.chattingService = chattingService;
+		this.orgService = orgService;
 	}
 	
 	@GetMapping("/chatRoomList/{emp_code}")
@@ -34,6 +40,13 @@ public class ChattingViewController {
 //		model.addAttribute("inviteList", inviteList);
 		
 		return "chatting/roomList";
+	}
+	
+	@ResponseBody
+	@GetMapping("/getOrgChartChat")
+	public ResponseEntity<List<Map<String, Object>>> getOrgChart() {
+	    List<Map<String, Object>> orgNodes = orgService.getOrgTree();  // 조직도 데이터를 가져오는 메서드
+	    return ResponseEntity.ok(orgNodes);  // JSON 형태로 반환
 	}
 	
 	@GetMapping("/chatDetail/{room_no}")
