2794040ecdf9f0f90829904c666b58c435f64ce1
cho <rudvkf2001@gmail.com>
2024-10-06 19:11:35 +0900

    삭제

--- Connet_Caffeine3/src/main/java/com/cc/job/repository/JobRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cc.job.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cc.job.domain.Job;
-
-public interface JobRepository extends JpaRepository<Job, String>{
-	Job findByjobCode(String job_code);
-}
