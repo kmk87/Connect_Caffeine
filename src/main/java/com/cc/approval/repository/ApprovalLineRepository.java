c3abec9915204e822584bd0ef05263f9478b1d7d
kmk87 <minkyoungk24@gmail.com>
2024-10-06 15:15:34 +0900

    [김민경] 전자결재 - 피드백 반영중_결재문서함 수정(24.10.06)

--- src/main/java/com/cc/approval/repository/ApprovalLineRepository.java
+++ src/main/java/com/cc/approval/repository/ApprovalLineRepository.java
@@ -91,7 +91,7 @@ public interface ApprovalLineRepository extends JpaRepository<ApprovalLine, Long
 
 
 	// 결재자로 등록된 모든 문서 리스트 가져오기
-	@Query("SELECT al FROM ApprovalLine al JOIN al.approval a WHERE al.employee.empCode = :empCode AND al.apprState IN ('S', 'C', 'R') ORDER BY a.draftDay DESC, al.approval.apprNo DESC")
+	@Query("SELECT al FROM ApprovalLine al JOIN al.approval a WHERE al.employee.empCode = :empCode AND al.apprState IN ('S', 'C', 'R') AND al.apprRole = 1 ORDER BY a.draftDay DESC, al.approval.apprNo DESC")
 	List<ApprovalLine> findAllApprovalLinesByEmpCode(@Param("empCode") Long empCode);
 
 
