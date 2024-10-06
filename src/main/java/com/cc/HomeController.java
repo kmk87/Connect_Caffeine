c528f8c9c1bc2ab7e22d6c7c8cf69aefb7f54924
Jongdam99 <jongdam99@gmail.com>
2024-10-07 00:16:42 +0900

    [이종담] 메인페이지 예약현황(2024.10.07)

--- src/main/java/com/cc/HomeController.java
+++ src/main/java/com/cc/HomeController.java
@@ -1,6 +1,8 @@
 package com.cc;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,6 +15,10 @@ import com.cc.attendance.domain.AttendanceDto;
 import com.cc.attendance.service.AttendanceService;
 import com.cc.employee.domain.EmployeeDto;
 import com.cc.employee.service.EmployeeService;
+import com.cc.reservation.domain.MeetingRoomDto;
+import com.cc.reservation.domain.ReservationDto;
+import com.cc.reservation.service.MeetingRoomService;
+import com.cc.reservation.service.ReservationService;
 import com.cc.tree.service.OrgService;
 @Controller
 public class HomeController {
@@ -20,13 +26,16 @@ public class HomeController {
 	private final EmployeeService employeeService;
 	private final OrgService orgService;
 	private final AttendanceService attendanceService;
-
+	private final ReservationService reservationService;
+	private final MeetingRoomService meetingRoomService;
 	@Autowired
-	public HomeController(EmployeeService employeeService, OrgService orgService, AttendanceService attendanceService) {
+	public HomeController(EmployeeService employeeService, OrgService orgService, AttendanceService attendanceService, ReservationService reservationService,MeetingRoomService meetingRoomService) {
 
 		this.employeeService = employeeService;
 		this.orgService = orgService;
 		this.attendanceService = attendanceService;
+		this.reservationService = reservationService;
+		this.meetingRoomService = meetingRoomService;
 	}
 
 	@GetMapping({ "", "/" })
@@ -44,7 +53,12 @@ public class HomeController {
 		// 2. 오늘 날짜 출퇴근
 		AttendanceDto attnDto = attendanceService.getTodayAttendance(empCode);
         model.addAttribute("attnDto", attnDto);
-
+        
+        // 예약 정보
+        List<ReservationDto> reservations = reservationService.getAllReservations();
+        List<MeetingRoomDto> meeting = meetingRoomService.selectMeetingRoomList();
+        model.addAttribute("meetings",meeting);
+        model.addAttribute("reservations", reservations);  // reservations로 수정
 		
 		return "index";
 	}
